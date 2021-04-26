c07242440340617bd72ce66a6ffb7f0fcd0db50b
survivingloop <aman.sethi.work@gmail.com>
2021-04-25 18:21:26 -0700

    Cleaning up redundant code in main.py

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -11,7 +11,6 @@
 from turtle import Screen
 from snake import *
 import time
-import functools
 
 
 def init_snake():
@@ -22,21 +21,6 @@ def init_snake():
           f"Food position = {snake.current_food.get_current_position()}")
     game_window.update()
     return snake
-
-
-def onkey_event_handler(key):
-    print(f"Inside onkey_event_handle. Key = {key}")
-    if key == UP_KEY:
-        my_snake.set_direction(UP)
-    elif key == DOWN_KEY:
-        my_snake.set_direction(DOWN)
-    elif key == LEFT_KEY:
-        my_snake.set_direction(LEFT)
-    elif key == RIGHT_KEY:
-        my_snake.set_direction(RIGHT)
-    elif key == "space":
-        my_snake.move_one_step()
-        game_window.update()
 
 
 def onkey_up():
@@ -59,11 +43,6 @@ def onkey_right():
     my_snake.set_direction(RIGHT)
 
 
-def onkey_space():
-    my_snake.move_one_step()
-    game_window.update()
-
-
 UP_KEY = "Up"
 DOWN_KEY = "Down"
 RIGHT_KEY = "Right"
@@ -75,18 +54,11 @@ game_window.bgcolor("black")
 game_window.title("Snake!!")
 my_snake = init_snake()
 
-# game_window.onkey(key=UP_KEY, fun=functools.partial(onkey_event_handler, UP_KEY))
-# game_window.onkey(key=DOWN_KEY, fun=functools.partial(onkey_event_handler, DOWN_KEY))
-# game_window.onkey(key=LEFT_KEY, fun=functools.partial(onkey_event_handler, LEFT_KEY))
-# game_window.onkey(key=RIGHT_KEY, fun=functools.partial(onkey_event_handler, RIGHT_KEY))
-# game_window.onkey(key="space", fun=functools.partial(onkey_event_handler, "space"))
-
 game_window.listen()
 game_window.onkey(key=UP_KEY, fun=onkey_up)
 game_window.onkey(key=DOWN_KEY, fun=onkey_down)
 game_window.onkey(key=RIGHT_KEY, fun=onkey_right)
 game_window.onkey(key=LEFT_KEY, fun=onkey_left)
-game_window.onkey(key="space", fun=onkey_space)
 
 while True:
     time.sleep(0.1)
